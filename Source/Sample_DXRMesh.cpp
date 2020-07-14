8369d35b998f4c4f85d8635247341aa76c6b5016
Adam Johnson <aj@nowhere.com>
2020-07-13 17:12:58 -0700

    Complete the basic implementation of a scene with instancing.

--- Source/Sample_DXRMesh.cpp
+++ Source/Sample_DXRMesh.cpp
@@ -14,6 +14,38 @@
 #include <atlbase.h>
 #include <array>
 #include <memory>
+#include <vector>
+
+class Instance
+{
+public:
+    matrix44 Transform;
+    uint32_t GeometryIndex;
+};
+
+class InstanceTable
+{
+public:
+    //uint32_t addMaterial(std::shared_ptr<Material> material);
+    uint32_t addMesh(std::shared_ptr<Mesh> mesh);
+    uint32_t addInstance(const matrix44& transform, uint32_t geometryIndex);
+    std::vector<std::shared_ptr<Mesh>> Meshes;
+    std::vector<Instance> Instances;
+};
+
+uint32_t InstanceTable::addMesh(std::shared_ptr<Mesh> mesh)
+{
+    uint32_t index = Meshes.size();
+    Meshes.push_back(mesh);
+    return index;
+}
+
+uint32_t InstanceTable::addInstance(const matrix44& transform, uint32_t geometryIndex)
+{
+    uint32_t index = Instances.size();
+    Instances.push_back({ transform, geometryIndex });
+    return index;
+}
 
 class Sample_DXRMesh : public Sample_DXRBase
 {
@@ -101,57 +133,70 @@ public:
     void RenderSample() override
     {
         ////////////////////////////////////////////////////////////////////////////////
-        // Create some simple geometry.
+        // Create a simple scene.
         ////////////////////////////////////////////////////////////////////////////////
-        std::shared_ptr<ParametricUV> shape(new Plane());
-        std::shared_ptr<Mesh> mesh(new ParametricUVToMesh(shape, 100, 100));
-        CComPtr<ID3D12Resource> Vertices;
-        CComPtr<ID3D12Resource> Indices;
-        {
-            {
-                int sizeVertex = sizeof(float[3]) * mesh->getVertexCount();
-                std::unique_ptr<int8_t[]> vertices(new int8_t[sizeVertex]);
-                mesh->copyVertices(reinterpret_cast<float3*>(vertices.get()), sizeof(float3));
-                Vertices.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeVertex, sizeVertex, vertices.get());
-            }
-            {
-                int sizeIndices = sizeof(int32_t) * mesh->getIndexCount();
-                std::unique_ptr<int8_t[]> indices(new int8_t[sizeIndices]);
-                mesh->copyIndices(reinterpret_cast<uint32_t*>(indices.get()), sizeof(uint32_t));
-                Indices.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeIndices, sizeIndices, indices.get());
-            }
-        }
+        std::shared_ptr<InstanceTable> scene(new InstanceTable());
+        std::shared_ptr<ParametricUV> _shape(new Plane());
+        std::shared_ptr<Mesh> _mesh(new ParametricUVToMesh(_shape, 100, 100));
+        uint32_t meshHandle = scene->addMesh(_mesh);
+        matrix44 transform = {};
+        transform.M11 = 1;
+        transform.M22 = 1;
+        transform.M33 = 1;
+        transform.M44 = 1;
+        transform.M41 = -1;
+        scene->addInstance(transform, meshHandle);
+        std::shared_ptr<Mesh> mesh2(new ParametricUVToMesh(_shape, 1, 1));
+        meshHandle = scene->addMesh(mesh2);
+        transform.M41 = 1;
+        scene->addInstance(transform, meshHandle);
         ////////////////////////////////////////////////////////////////////////////////
-        // BLAS - Build the bottom level acceleration structure.
+        // BLAS - Build the bottom level acceleration structures.
         ////////////////////////////////////////////////////////////////////////////////
         // Create and initialize the BLAS.
-        CComPtr<ID3D12Resource1> ResourceBLAS;
+        std::vector<CComPtr<ID3D12Resource1>> BottomLevelAS;
+        BottomLevelAS.resize(scene->Meshes.size());
+        for (int i = 0; i < scene->Meshes.size(); ++i)
         {
+            CComPtr<ID3D12Resource> vertexBuffer;
+            CComPtr<ID3D12Resource> indexBuffer;
+            {
+                int sizeVertex = sizeof(float[3]) * scene->Meshes[i]->getVertexCount();
+                std::unique_ptr<int8_t[]> vertices(new int8_t[sizeVertex]);
+                scene->Meshes[i]->copyVertices(reinterpret_cast<float3*>(vertices.get()), sizeof(float3));
+                vertexBuffer.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeVertex, sizeVertex, vertices.get());
+            }
+            {
+                int sizeIndices = sizeof(int32_t) * scene->Meshes[i]->getIndexCount();
+                std::unique_ptr<int8_t[]> indices(new int8_t[sizeIndices]);
+                scene->Meshes[i]->copyIndices(reinterpret_cast<uint32_t*>(indices.get()), sizeof(uint32_t));
+                indexBuffer.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeIndices, sizeIndices, indices.get());
+            }
             D3D12_RAYTRACING_ACCELERATION_STRUCTURE_PREBUILD_INFO descRaytracingPrebuild = {};
             D3D12_BUILD_RAYTRACING_ACCELERATION_STRUCTURE_INPUTS descRaytracingInputs = {};
             descRaytracingInputs.Type = D3D12_RAYTRACING_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL;
             descRaytracingInputs.NumDescs = 1;
             descRaytracingInputs.DescsLayout = D3D12_ELEMENTS_LAYOUT_ARRAY;
-            D3D12_RAYTRACING_GEOMETRY_DESC descGeometry[1] = {};
-            descGeometry[0].Type = D3D12_RAYTRACING_GEOMETRY_TYPE_TRIANGLES;
-            descGeometry[0].Flags = D3D12_RAYTRACING_GEOMETRY_FLAG_OPAQUE;
-            descGeometry[0].Triangles.IndexFormat = DXGI_FORMAT_R32_UINT;
-            descGeometry[0].Triangles.VertexFormat = DXGI_FORMAT_R32G32B32_FLOAT;
-            descGeometry[0].Triangles.IndexCount = mesh->getIndexCount();
-            descGeometry[0].Triangles.VertexCount = mesh->getVertexCount();
-            descGeometry[0].Triangles.IndexBuffer = Indices->GetGPUVirtualAddress();
-            descGeometry[0].Triangles.VertexBuffer.StartAddress = Vertices->GetGPUVirtualAddress();
-            descGeometry[0].Triangles.VertexBuffer.StrideInBytes = sizeof(float[3]);
-            descRaytracingInputs.pGeometryDescs = &descGeometry[0];
+            D3D12_RAYTRACING_GEOMETRY_DESC descGeometry = {};
+            descGeometry.Type = D3D12_RAYTRACING_GEOMETRY_TYPE_TRIANGLES;
+            descGeometry.Flags = D3D12_RAYTRACING_GEOMETRY_FLAG_OPAQUE;
+            descGeometry.Triangles.IndexFormat = DXGI_FORMAT_R32_UINT;
+            descGeometry.Triangles.VertexFormat = DXGI_FORMAT_R32G32B32_FLOAT;
+            descGeometry.Triangles.IndexCount = scene->Meshes[i]->getIndexCount();
+            descGeometry.Triangles.VertexCount = scene->Meshes[i]->getVertexCount();
+            descGeometry.Triangles.IndexBuffer = indexBuffer->GetGPUVirtualAddress();
+            descGeometry.Triangles.VertexBuffer.StartAddress = vertexBuffer->GetGPUVirtualAddress();
+            descGeometry.Triangles.VertexBuffer.StrideInBytes = sizeof(float[3]);
+            descRaytracingInputs.pGeometryDescs = &descGeometry;
             m_pDevice->GetID3D12Device()->GetRaytracingAccelerationStructurePrebuildInfo(&descRaytracingInputs, &descRaytracingPrebuild);
             // Create the output and scratch buffers.
-            ResourceBLAS.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_RAYTRACING_ACCELERATION_STRUCTURE, descRaytracingPrebuild.ResultDataMaxSizeInBytes);
+            BottomLevelAS[i].p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_RAYTRACING_ACCELERATION_STRUCTURE, descRaytracingPrebuild.ResultDataMaxSizeInBytes);
             CComPtr<ID3D12Resource1> ResourceASScratch;
             ResourceASScratch.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_UNORDERED_ACCESS, descRaytracingPrebuild.ResultDataMaxSizeInBytes);
             // Build the acceleration structure.
             RunOnGPU(m_pDevice, [&](ID3D12GraphicsCommandList5* UploadBLASCommandList) {
                 D3D12_BUILD_RAYTRACING_ACCELERATION_STRUCTURE_DESC descBuild = {};
-                descBuild.DestAccelerationStructureData = ResourceBLAS->GetGPUVirtualAddress();
+                descBuild.DestAccelerationStructureData = BottomLevelAS[i]->GetGPUVirtualAddress();
                 descBuild.Inputs = descRaytracingInputs;
                 descBuild.ScratchAccelerationStructureData = ResourceASScratch->GetGPUVirtualAddress();
                 UploadBLASCommandList->BuildRaytracingAccelerationStructure(&descBuild, 0, nullptr);
@@ -162,13 +207,28 @@ public:
         ////////////////////////////////////////////////////////////////////////////////
         CComPtr<ID3D12Resource1> ResourceInstance;
         {
-            D3D12_RAYTRACING_INSTANCE_DESC DxrInstance[1] = {};
-            DxrInstance[0].Transform[0][0] = 1;
-            DxrInstance[0].Transform[1][1] = 1;
-            DxrInstance[0].Transform[2][2] = 1;
-            DxrInstance[0].InstanceMask = 0xFF;
-            DxrInstance[0].AccelerationStructure = ResourceBLAS->GetGPUVirtualAddress();
-            ResourceInstance.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeof(DxrInstance), sizeof(DxrInstance), &DxrInstance);
+            std::vector<D3D12_RAYTRACING_INSTANCE_DESC> instanceDescs;
+            instanceDescs.resize(scene->Instances.size());
+            for (int i = 0; i < scene->Instances.size(); ++i)
+            {
+                instanceDescs[i].Transform[0][0] = scene->Instances[i].Transform.M11;
+                instanceDescs[i].Transform[0][1] = scene->Instances[i].Transform.M21;
+                instanceDescs[i].Transform[0][2] = scene->Instances[i].Transform.M31;
+                instanceDescs[i].Transform[0][3] = scene->Instances[i].Transform.M41;
+                instanceDescs[i].Transform[1][0] = scene->Instances[i].Transform.M12;
+                instanceDescs[i].Transform[1][1] = scene->Instances[i].Transform.M22;
+                instanceDescs[i].Transform[1][2] = scene->Instances[i].Transform.M32;
+                instanceDescs[i].Transform[1][3] = scene->Instances[i].Transform.M42;
+                instanceDescs[i].Transform[2][0] = scene->Instances[i].Transform.M13;
+                instanceDescs[i].Transform[2][1] = scene->Instances[i].Transform.M23;
+                instanceDescs[i].Transform[2][2] = scene->Instances[i].Transform.M33;
+                instanceDescs[i].Transform[2][3] = scene->Instances[i].Transform.M43;
+                instanceDescs[i].InstanceID = i;
+                instanceDescs[i].InstanceMask = 0xFF;
+                //instanceDescs[i].InstanceContributionToHitGroupIndex = scene->Instances[i].MaterialIndex;
+                instanceDescs[i].AccelerationStructure = BottomLevelAS[scene->Instances[i].GeometryIndex]->GetGPUVirtualAddress();
+            }
+            ResourceInstance.p = D3D12CreateBuffer(m_pDevice, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_COMMON, sizeof(D3D12_RAYTRACING_INSTANCE_DESC) * scene->Instances.size(), sizeof(D3D12_RAYTRACING_INSTANCE_DESC) * scene->Instances.size(), &instanceDescs[0]);
         }
         ////////////////////////////////////////////////////////////////////////////////
         // TLAS - Build the top level acceleration structure.
@@ -178,7 +238,7 @@ public:
             D3D12_RAYTRACING_ACCELERATION_STRUCTURE_PREBUILD_INFO descRaytracingPrebuild = {};
             D3D12_BUILD_RAYTRACING_ACCELERATION_STRUCTURE_INPUTS descRaytracingInputs = {};
             descRaytracingInputs.Type = D3D12_RAYTRACING_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL;
-            descRaytracingInputs.NumDescs = 1;
+            descRaytracingInputs.NumDescs = scene->Instances.size();
             descRaytracingInputs.DescsLayout = D3D12_ELEMENTS_LAYOUT_ARRAY;
             descRaytracingInputs.InstanceDescs = ResourceInstance->GetGPUVirtualAddress();
             m_pDevice->GetID3D12Device()->GetRaytracingAccelerationStructurePrebuildInfo(&descRaytracingInputs, &descRaytracingPrebuild);
--- Source/Scene_Plane.cpp
+++ Source/Scene_Plane.cpp
@@ -3,5 +3,7 @@
 
 float3 Plane::getVertexPosition(float2 uv)
 {
-    return { -1 + uv.X * 2, sinf(uv.X * 20.0f) * 0.25f, -1 + uv.Y * 2 };
+    static float phase = 0;
+    phase += 0.00001f;
+    return { -1 + uv.X * 2, sinf(uv.X * 20.0f + phase) * 0.25f, -1 + uv.Y * 2 };
 }
