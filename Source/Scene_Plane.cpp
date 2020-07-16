a08eb09862323e3fb4201285d94e1e471f419b14
Adam Johnson <aj@nowhere.com>
2020-07-15 22:28:34 -0700

    Junky animation warning.

--- Source/Scene_Plane.cpp
+++ Source/Scene_Plane.cpp
@@ -3,6 +3,7 @@
 
 float3 Plane::getVertexPosition(float2 uv)
 {
+    // NOTE: We're animating on every vertex lookup - this is horrible but it's fun to look at.
     static float phase = 0;
     phase += 0.00001f;
     return { -1 + uv.X * 2, sinf(uv.X * 20.0f + phase), -1 + uv.Y * 2 };
