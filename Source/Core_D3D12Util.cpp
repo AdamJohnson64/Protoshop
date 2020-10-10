c0c720fcdaa32ba2d5880702d98ab99cc4e6e3cc
Adam Johnson <aj@nowhere.com>
2020-10-09 22:04:26 -0700

    Clean up buffer management code.

--- Source/Core_D3D12Util.cpp
+++ Source/Core_D3D12Util.cpp
@@ -10,10 +10,8 @@ uint32_t D3D12Align(uint32_t size, uint32_t alignSize)
     return size == 0 ? 0 : ((size - 1) / alignSize + 1) * alignSize;
 }
 
-ID3D12Resource1* D3D12CreateBuffer(std::shared_ptr<Direct3D12Device> device, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state, uint32_t bufferSize)
+ID3D12Resource1* D3D12CreateBuffer(std::shared_ptr<Direct3D12Device> device, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state, uint32_t bufferSize, const D3D12_HEAP_PROPERTIES* heap)
 {
-    D3D12_HEAP_PROPERTIES descHeapProperties = {};
-    descHeapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
     D3D12_RESOURCE_DESC descResource = {};
     descResource.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
     descResource.Width = bufferSize;
@@ -23,45 +21,48 @@ ID3D12Resource1* D3D12CreateBuffer(std::shared_ptr<Direct3D12Device> device, D3D
     descResource.SampleDesc.Count = 1;
     descResource.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
     descResource.Flags = flags;
-    ID3D12Resource1* result = nullptr;
-    TRYD3D(device->GetID3D12Device()->CreateCommittedResource(&descHeapProperties, D3D12_HEAP_FLAG_NONE, &descResource, state, nullptr, __uuidof(ID3D12Resource1), (void**)&result));
-    TRYD3D(result->SetName(L"D3D12CreateBuffer"));
-    return result;
+    CComPtr<ID3D12Resource1> pD3D12Resource;
+    TRYD3D(device->GetID3D12Device()->CreateCommittedResource(heap, D3D12_HEAP_FLAG_NONE, &descResource, state, nullptr, __uuidof(ID3D12Resource1), (void**)&pD3D12Resource.p));
+    TRYD3D(pD3D12Resource->SetName(L"D3D12CreateBuffer"));
+    return pD3D12Resource.Detach();
+}
+
+ID3D12Resource1* D3D12CreateBuffer(std::shared_ptr<Direct3D12Device> device, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state, uint32_t bufferSize)
+{
+    D3D12_HEAP_PROPERTIES descHeapDefault = {};
+    descHeapDefault.Type = D3D12_HEAP_TYPE_DEFAULT;
+    return D3D12CreateBuffer(device, flags, state, bufferSize, &descHeapDefault);
 }
 
 ID3D12Resource1* D3D12CreateBuffer(std::shared_ptr<Direct3D12Device> device, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state, uint32_t bufferSize, uint32_t dataSize, const void* data)
 {
-    ID3D12Resource1* result = D3D12CreateBuffer(device, flags, D3D12_RESOURCE_STATE_COPY_DEST, bufferSize);
-    CComPtr<ID3D12Resource1> upload;
-    D3D12_HEAP_PROPERTIES descHeapProperties = {};
-    descHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
-    D3D12_RESOURCE_DESC descResource = {};
-    descResource.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-    descResource.Width = bufferSize;
-    descResource.Height = 1;
-    descResource.DepthOrArraySize = 1;
-    descResource.MipLevels = 1;
-    descResource.SampleDesc.Count = 1;
-    descResource.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-    TRYD3D(device->GetID3D12Device()->CreateCommittedResource(&descHeapProperties, D3D12_HEAP_FLAG_NONE, &descResource, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, __uuidof(ID3D12Resource1), (void**)&upload));
-    TRYD3D(upload->SetName(L"D3D12CreateBuffer_UPLOAD"));
-    void *pTLAS = nullptr;
-    TRYD3D(upload->Map(0, nullptr, &pTLAS));
-    memcpy(pTLAS, data, dataSize);
-    upload->Unmap(0, nullptr);
-    // Copy this staging buffer to the GPU-only buffer.
-    RunOnGPU(device, [&](ID3D12GraphicsCommandList5* uploadCommandList) {
-        uploadCommandList->CopyResource(result, upload);
+    CComPtr<ID3D12Resource1> pD3D12Resource;
+    pD3D12Resource.p = D3D12CreateBuffer(device, flags, D3D12_RESOURCE_STATE_COPY_DEST, bufferSize);
+    {
+        CComPtr<ID3D12Resource1> pD3D12ResourceUpload;
         {
-            D3D12_RESOURCE_BARRIER descBarrier = {};
-            descBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-            descBarrier.Transition.pResource = result;
-            descBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
-            descBarrier.Transition.StateAfter = state;
-            uploadCommandList->ResourceBarrier(1, &descBarrier);
+            D3D12_HEAP_PROPERTIES descHeapUpload = {};
+            descHeapUpload.Type = D3D12_HEAP_TYPE_UPLOAD;
+            pD3D12ResourceUpload.p = D3D12CreateBuffer(device, D3D12_RESOURCE_FLAG_NONE, D3D12_RESOURCE_STATE_GENERIC_READ, bufferSize, &descHeapUpload);
         }
-    });
-    return result;
+        void *pMapped = nullptr;
+        TRYD3D(pD3D12ResourceUpload->Map(0, nullptr, &pMapped));
+        memcpy(pMapped, data, dataSize);
+        pD3D12ResourceUpload->Unmap(0, nullptr);
+        // Copy this staging buffer to the GPU-only buffer.
+        RunOnGPU(device, [&](ID3D12GraphicsCommandList5* uploadCommandList) {
+            uploadCommandList->CopyResource(pD3D12Resource, pD3D12ResourceUpload);
+            {
+                D3D12_RESOURCE_BARRIER descBarrier = {};
+                descBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+                descBarrier.Transition.pResource = pD3D12Resource;
+                descBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+                descBarrier.Transition.StateAfter = state;
+                uploadCommandList->ResourceBarrier(1, &descBarrier);
+            }
+        });
+    }
+    return pD3D12Resource.Detach();
 }
 
 void D3D12WaitForGPUIdle(std::shared_ptr<Direct3D12Device> device)
