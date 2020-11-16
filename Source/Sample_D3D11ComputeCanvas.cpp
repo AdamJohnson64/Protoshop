ad299fba09e0ab575094ad50418e5d071c12a267
Adam Johnson <aj@nowhere.com>
2020-11-15 22:49:00 -0800

    Use Tex2D in compute canvas sample.
    Using a UAV to access the image is cute, but it's very limiting as it doesn't support pixel formats and needs extra metadata passed into the shader. We should use an SRV.

--- Source/Sample_D3D11ComputeCanvas.cpp
+++ Source/Sample_D3D11ComputeCanvas.cpp
@@ -25,15 +25,10 @@ class Sample_D3D11ComputeCanvas : public Sample
 private:
     std::shared_ptr<DXGISwapChain> m_pSwapChain;
     std::shared_ptr<Direct3D11Device> m_pDevice;
-    CComPtr<ID3D11ComputeShader> m_pD3D11ComputeShader;
-    CComPtr<ID3D11Buffer> m_pD3D11BufferConstants;
-    CComPtr<ID3D11Buffer> m_pD3D11BufferImage;
-    CComPtr<ID3D11UnorderedAccessView> m_pD3D11UnorderedAccessViewImage;
-    struct Constants
-    {
-        Matrix44 Transform;
-        int Width, Height;
-    };
+    CComPtr<ID3D11ComputeShader> m_pComputeShader;
+    CComPtr<ID3D11Buffer> m_pBufferConstants;
+    CComPtr<ID3D11Texture2D> m_pTex2DImage;
+    CComPtr<ID3D11ShaderResourceView> m_pSRVImage;
 public:
     Sample_D3D11ComputeCanvas(std::shared_ptr<DXGISwapChain> pSwapChain, std::shared_ptr<Direct3D11Device> pDevice) :
         m_pSwapChain(pSwapChain),
@@ -44,12 +39,10 @@ public:
 cbuffer Constants
 {
     float4x4 transform;
-    int width;
-    int height;
 };
 
 RWTexture2D<float4> renderTarget;
-RWBuffer<uint> buffer;
+Texture2D<float4> userImage;
 
 float4 Checkerboard(uint2 pixel)
 {
@@ -57,35 +50,15 @@ float4 Checkerboard(uint2 pixel)
     return lerp(float4(0.25, 0.25, 0.25, 1), float4(0.75, 0.75, 0.75, 1), modulo);
 }
 
-float4 unpackPixel(uint p)
-{
-    float4 result;
-    result.r = ((p >> 0) & 0xFF) / 255.0f;
-    result.g = ((p >> 8) & 0xFF) / 255.0f;
-    result.b = ((p >> 16) & 0xFF) / 255.0f;
-    result.a = ((p >> 24) & 0xFF) / 255.0f;
-    return result;
-}
-
-float4 UserImage(float2 pixel)
-{
-    int2 iPixel = (int2)pixel;
-    return unpackPixel(buffer[iPixel.x + iPixel.y * width]);    
-
-    float2 fPixel = (float2)pixel - float2(128, 128);
-    float dist = sqrt(dot(fPixel, fPixel));
-    return (dist > 96 && dist < 112) ? float4(1, 0, 0, 0) : float4(0, 0, 0, 0);
-    
-    //return float4(1, 0, 0, 1);
-}
-
 float4 Sample(float2 pixel)
 {
     float4 checkerboard = Checkerboard((uint2)pixel);
     float2 pos = mul(transform, float4(pixel.x, pixel.y, 0, 1)).xy;
-    if (pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height)
+    uint Width = 16, Height = 16, NumberOfLevels;
+    userImage.GetDimensions(0, Width, Height, NumberOfLevels);
+    if (pos.x >= 0 && pos.x < (int)Width && pos.y >= 0 && pos.y < (int)Height)
     {
-        return UserImage((float2)pos);
+        return userImage.Load(int3(pos.x, pos.y, 0));
     }
     else
     {
@@ -96,33 +69,43 @@ float4 Sample(float2 pixel)
 [numthreads(1, 1, 1)]
 void main(uint3 dispatchThreadId : SV_DispatchThreadID)
 {
+    ////////////////////////////////////////////////////////////////////////////////
+    // No Supersampling.
     renderTarget[dispatchThreadId.xy] = Sample((float2)dispatchThreadId);
     return;
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    // 8x8 (64 TAP) Supersampling.
+    const int superCountX = 8;
+    const int superCountY = 8;
+    const float superSpacingX = 1.0 / superCountX;
+    const float superSpacingY = 1.0 / superCountY;
+    const float superOffsetX = superSpacingX / 2;
+    const float superOffsetY = superSpacingY / 2;
     float4 accumulateSamples;
-    for (int superSampleY = 0; superSampleY < 4; ++superSampleY)
+    for (int superSampleY = 0; superSampleY < superCountY; ++superSampleY)
     {
-        for (int superSampleX = 0; superSampleX < 4; ++superSampleX)
+        for (int superSampleX = 0; superSampleX < superCountX; ++superSampleX)
         {
-            accumulateSamples += Sample(float2(dispatchThreadId.x + 0.125 + superSampleX * 0.25, dispatchThreadId.y + 0.125 + superSampleY * 0.25));
+            accumulateSamples += Sample(float2(
+                dispatchThreadId.x + superOffsetX + superSampleX * superSpacingX,
+                dispatchThreadId.y + superOffsetY + superSampleY * superSpacingY));
         }
     }
-    accumulateSamples /= 16;
+    accumulateSamples /= superCountX * superCountY;
     renderTarget[dispatchThreadId.xy] = accumulateSamples;
 })SHADER");
         {
-            D3D11_BUFFER_DESC descBuffer = {};
-            descBuffer.ByteWidth = 1024;
-            descBuffer.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-            descBuffer.StructureByteStride = sizeof(Constants);
-            TRYD3D(m_pDevice->GetID3D11Device()->CreateBuffer(&descBuffer, nullptr, &m_pD3D11BufferConstants));
+            D3D11_BUFFER_DESC desc = {};
+            desc.ByteWidth = 1024;
+            desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+            desc.StructureByteStride = sizeof(Matrix44);
+            TRYD3D(m_pDevice->GetID3D11Device()->CreateBuffer(&desc, nullptr, &m_pBufferConstants));
         }
         {
-            D3D11_BUFFER_DESC descBuffer = {};
-            descBuffer.ByteWidth = sizeof(uint32_t) * IMAGE_WIDTH * IMAGE_HEIGHT;
-            descBuffer.BindFlags = D3D11_BIND_UNORDERED_ACCESS;
-            descBuffer.StructureByteStride = sizeof(uint32_t);
-            uint32_t data[IMAGE_WIDTH * IMAGE_HEIGHT];
-            char stamp[] =
+            struct Pixel { uint8_t B, G, R, A; };
+            Pixel imageRaw[IMAGE_WIDTH * IMAGE_HEIGHT];
+            char imageStamp[] =
             {
                 0, 0, 0, 1, 1, 0, 0, 0,
                 0, 0, 1, 0, 0, 1, 0, 0,
@@ -133,73 +116,85 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID)
                 0, 1, 0, 0, 0, 0, 1, 0,
                 0, 1, 0, 0, 0, 0, 1, 0,
             };
-            for (int a = 0; a < IMAGE_WIDTH * IMAGE_HEIGHT; ++a)
+            for (int i = 0; i < IMAGE_WIDTH * IMAGE_HEIGHT; ++i)
             {
-                data[a] = 0xFF0080FF + a;
+                Pixel& pixel = imageRaw[i];
+                pixel.B = 0x00;
+                pixel.G = 0x00;
+                pixel.R = i >> 4;
+                pixel.A = 0xFF;
             }
             for (int y = 0; y < 8; ++y)
             {
                 for (int x = 0; x < 8; ++x)
                 {
-                    if (stamp[x + y * 8])
+                    if (imageStamp[x + y * 8])
                     {
-                        data[(16 + x) + (16 + y) * IMAGE_WIDTH] = 0xFFFF0000;
+                        Pixel& pixel = imageRaw[(16 + x) + (16 + y) * IMAGE_WIDTH];
+                        pixel.B = 0x00;
+                        pixel.G = 0xFF;
+                        pixel.R = 0xFF;
+                        pixel.A = 0xFF;
                     }
                 }
             }
-            D3D11_SUBRESOURCE_DATA descData = {};
-            descData.pSysMem = data;
-            TRYD3D(m_pDevice->GetID3D11Device()->CreateBuffer(&descBuffer, &descData, &m_pD3D11BufferImage));
+            {
+                D3D11_TEXTURE2D_DESC desc = {};
+                desc.Width = IMAGE_WIDTH;
+                desc.Height = IMAGE_HEIGHT;
+                desc.MipLevels = 1;
+                desc.ArraySize = 1;
+                desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+                desc.SampleDesc.Count = 1;
+                desc.Usage = D3D11_USAGE_IMMUTABLE;
+                desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+                D3D11_SUBRESOURCE_DATA descData = {};
+                descData.pSysMem = imageRaw;
+                descData.SysMemPitch = sizeof(Pixel) * IMAGE_WIDTH;
+                descData.SysMemSlicePitch = sizeof(Pixel) * IMAGE_WIDTH * IMAGE_HEIGHT;
+                TRYD3D(m_pDevice->GetID3D11Device()->CreateTexture2D(&desc, &descData, &m_pTex2DImage.p));
+            }
         }
         {
-            D3D11_UNORDERED_ACCESS_VIEW_DESC descUAV = {};
-            descUAV.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
-            descUAV.Format = DXGI_FORMAT_R32_UINT;
-            descUAV.Buffer.NumElements = IMAGE_WIDTH * IMAGE_HEIGHT;
-            TRYD3D(m_pDevice->GetID3D11Device()->CreateUnorderedAccessView(m_pD3D11BufferImage, &descUAV, &m_pD3D11UnorderedAccessViewImage));
+            D3D11_SHADER_RESOURCE_VIEW_DESC desc = {};
+            desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+            desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+            desc.Texture2D.MipLevels = 1;
+            TRYD3D(m_pDevice->GetID3D11Device()->CreateShaderResourceView(m_pTex2DImage, &desc, &m_pSRVImage.p));
         }
-        TRYD3D(m_pDevice->GetID3D11Device()->CreateComputeShader(pD3DBlobCodeCS->GetBufferPointer(), pD3DBlobCodeCS->GetBufferSize(), nullptr, &m_pD3D11ComputeShader));
+        TRYD3D(m_pDevice->GetID3D11Device()->CreateComputeShader(pD3DBlobCodeCS->GetBufferPointer(), pD3DBlobCodeCS->GetBufferSize(), nullptr, &m_pComputeShader));
     }
     void Render()
     {
         // Get the backbuffer and create a render target from it.
-        CComPtr<ID3D11UnorderedAccessView> pD3D11UnorderedAccessViewRenderTarget;
+        CComPtr<ID3D11UnorderedAccessView> pUAVTarget;
         {
             CComPtr<ID3D11Texture2D> pD3D11Texture2D;
             TRYD3D(m_pSwapChain->GetIDXGISwapChain()->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pD3D11Texture2D));
             D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
             uavDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
             uavDesc.ViewDimension = D3D11_UAV_DIMENSION_TEXTURE2D;
-            TRYD3D(m_pDevice->GetID3D11Device()->CreateUnorderedAccessView(pD3D11Texture2D, &uavDesc, &pD3D11UnorderedAccessViewRenderTarget));
+            TRYD3D(m_pDevice->GetID3D11Device()->CreateUnorderedAccessView(pD3D11Texture2D, &uavDesc, &pUAVTarget));
         }
         // Upload the constant buffer.
-        static float a = 0;
-        Constants data = {};
-        data.Transform.M11 = data.Transform.M22 = data.Transform.M33 = data.Transform.M44 = 1;
-        data.Transform.M11 = cosf(a); data.Transform.M12 = sinf(a);
-        data.Transform.M21 = -sinf(a); data.Transform.M22 = cosf(a);
-        data.Transform.M41 = (RENDERTARGET_WIDTH - IMAGE_WIDTH * cosf(a) + IMAGE_HEIGHT * sinf(a)) / 2.0f;
-        data.Transform.M42 = (RENDERTARGET_HEIGHT - IMAGE_WIDTH * sinf(a) - IMAGE_HEIGHT * cosf(a)) / 2.0f;
-        data.Transform = Invert(data.Transform);
-        data.Width = IMAGE_WIDTH;
-        data.Height = IMAGE_HEIGHT;
-        a += 0.01f;
-        m_pDevice->GetID3D11DeviceContext()->UpdateSubresource(m_pD3D11BufferConstants, 0, nullptr, &data, 0, 0);
+        static float angle = 0;
+        Matrix44 transform = {};
+        transform.M11 = transform.M22 = transform.M33 = transform.M44 = 1;
+        transform.M11 = cosf(angle); transform.M12 = sinf(angle);
+        transform.M21 = -sinf(angle); transform.M22 = cosf(angle);
+        transform.M41 = (RENDERTARGET_WIDTH - IMAGE_WIDTH * cosf(angle) + IMAGE_HEIGHT * sinf(angle)) / 2.0f;
+        transform.M42 = (RENDERTARGET_HEIGHT - IMAGE_WIDTH * sinf(angle) - IMAGE_HEIGHT * cosf(angle)) / 2.0f;
+        transform = Invert(transform);
+        angle += 0.01f;
+        m_pDevice->GetID3D11DeviceContext()->ClearState();
+        m_pDevice->GetID3D11DeviceContext()->UpdateSubresource(m_pBufferConstants, 0, nullptr, &transform, 0, 0);
         // Beginning of rendering.
-        m_pDevice->GetID3D11DeviceContext()->CSSetShader(m_pD3D11ComputeShader, nullptr, 0);
-        m_pDevice->GetID3D11DeviceContext()->CSSetConstantBuffers(0, 1, &m_pD3D11BufferConstants.p);
-        m_pDevice->GetID3D11DeviceContext()->CSSetUnorderedAccessViews(0, 1, &pD3D11UnorderedAccessViewRenderTarget.p, nullptr);
-        m_pDevice->GetID3D11DeviceContext()->CSSetUnorderedAccessViews(1, 1, &m_pD3D11UnorderedAccessViewImage.p, nullptr);
+        m_pDevice->GetID3D11DeviceContext()->CSSetUnorderedAccessViews(0, 1, &pUAVTarget.p, nullptr);
+        m_pDevice->GetID3D11DeviceContext()->CSSetShader(m_pComputeShader, nullptr, 0);
+        m_pDevice->GetID3D11DeviceContext()->CSSetConstantBuffers(0, 1, &m_pBufferConstants.p);
+        m_pDevice->GetID3D11DeviceContext()->CSSetShaderResources(0, 1, &m_pSRVImage.p);
         m_pDevice->GetID3D11DeviceContext()->Dispatch(RENDERTARGET_WIDTH, RENDERTARGET_HEIGHT, 1);
-        ID3D11UnorderedAccessView* makeNullptr = nullptr;
-        m_pDevice->GetID3D11DeviceContext()->CSSetUnorderedAccessViews(0, 1, &makeNullptr, nullptr);
-        {
-            D3D11_VIEWPORT viewportdesc = {};
-            viewportdesc.Width = RENDERTARGET_WIDTH;
-            viewportdesc.Height = RENDERTARGET_HEIGHT;
-            viewportdesc.MaxDepth = 1.0f;
-            m_pDevice->GetID3D11DeviceContext()->RSSetViewports(1, &viewportdesc);
-        }
+        m_pDevice->GetID3D11DeviceContext()->ClearState();
         m_pDevice->GetID3D11DeviceContext()->Flush();
         
         // End of rendering; send to display.
