4cfe000e3d21717949688bbe5b33c20b8c39d41c
Adam Johnson <aj@nowhere.com>
2020-11-22 20:14:07 -0800

    Put D3D11 in hardware mode.
    WARP is starting to struggle a little...

--- Source/Core_D3D11.cpp
+++ Source/Core_D3D11.cpp
@@ -12,7 +12,7 @@ class Direct3D11DeviceImpl : public Direct3D11Device
 public:
     Direct3D11DeviceImpl()
     {
-        TRYD3D(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_WARP, nullptr, D3D11_CREATE_DEVICE_DEBUG, nullptr, 0, D3D11_SDK_VERSION, &pD3D11Device, nullptr, &pD3D11DeviceContext));
+        TRYD3D(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, nullptr, 0, D3D11_SDK_VERSION, &pD3D11Device, nullptr, &pD3D11DeviceContext));
     }
     ID3D11Device* GetID3D11Device()
     {
