de6cd1f5de7a4cc4e1d8a778e4d720643249ffb0
Adam Johnson <aj@nowhere.com>
2020-11-25 23:27:38 -0800

    Stop reloading the instance table every frame.
    Once PLYs end up in here this will become very expensive, very slow, and very dumb.

--- Source/Scene_InstanceTable.cpp
+++ Source/Scene_InstanceTable.cpp
@@ -6,37 +6,42 @@
 
 std::shared_ptr<InstanceTable> InstanceTable::Default()
 {
-    std::shared_ptr<InstanceTable> scene(new InstanceTable());
-    // Create Materials.
-    std::shared_ptr<Material> _checkerboard(new Checkerboard());
-    uint32_t hCheckerboard = scene->addMaterial(_checkerboard);
-    std::shared_ptr<Material> _plastic(new RedPlastic());
-    uint32_t hPlastic = scene->addMaterial(_plastic);
-    // Create Geometry.
-    std::shared_ptr<ParametricUV> _plane(new Plane());
-    std::shared_ptr<Mesh> _mesh(new ParametricUVToMesh(_plane, 100, 100));
-    uint32_t hPlane = scene->addMesh(_mesh);
-    std::shared_ptr<ParametricUV> _sphere(new Sphere());
-    std::shared_ptr<Mesh> _mesh2(new ParametricUVToMesh(_sphere, 100, 100));
-    // Create Instances.
-    Matrix44 transform = {};
-    transform.M11 = 10;
-    transform.M22 = 0.25f;
-    transform.M33 = 10;
-    transform.M44 = 1;
-    scene->addInstance(transform, hPlane, hCheckerboard);
-    uint32_t hSphere = scene->addMesh(_mesh2);
-    transform.M11 = 1;
-    transform.M22 = 1;
-    transform.M33 = 1;
-    transform.M44 = 1;
-    transform.M42 = 1;
-    transform.M41 = -2;
-    scene->addInstance(transform, hSphere, hPlastic);
-    transform.M41 = 0;
-    scene->addInstance(transform, hSphere, hPlastic);
-    transform.M41 = 2;
-    scene->addInstance(transform, hSphere, hPlastic);
+    static std::shared_ptr<InstanceTable> scene(new InstanceTable());
+    static bool initialized = false;
+    if (!initialized)
+    {
+        // Create Materials.
+        std::shared_ptr<Material> _checkerboard(new Checkerboard());
+        uint32_t hCheckerboard = scene->addMaterial(_checkerboard);
+        std::shared_ptr<Material> _plastic(new RedPlastic());
+        uint32_t hPlastic = scene->addMaterial(_plastic);
+        // Create Geometry.
+        std::shared_ptr<ParametricUV> _plane(new Plane());
+        std::shared_ptr<Mesh> _mesh(new ParametricUVToMesh(_plane, 100, 100));
+        uint32_t hPlane = scene->addMesh(_mesh);
+        std::shared_ptr<ParametricUV> _sphere(new Sphere());
+        std::shared_ptr<Mesh> _mesh2(new ParametricUVToMesh(_sphere, 100, 100));
+        uint32_t hSphere = scene->addMesh(_mesh2);
+        // Create Instances.
+        Matrix44 transform = {};
+        transform.M11 = 10;
+        transform.M22 = 0.25f;
+        transform.M33 = 10;
+        transform.M44 = 1;
+        scene->addInstance(transform, hPlane, hCheckerboard);
+        transform.M11 = 1;
+        transform.M22 = 1;
+        transform.M33 = 1;
+        transform.M44 = 1;
+        transform.M42 = 1;
+        transform.M41 = -2;
+        scene->addInstance(transform, hSphere, hPlastic);
+        transform.M41 = 0;
+        scene->addInstance(transform, hSphere, hPlastic);
+        transform.M41 = 2;
+        scene->addInstance(transform, hSphere, hPlastic);
+        initialized = true;
+    }
     return scene;
 }
 
