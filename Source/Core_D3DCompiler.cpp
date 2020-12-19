828db132a285d4ce693b36ad48b9bcec1cff0dc8
Adam Johnson <aj@nowhere.com>
2020-12-19 10:59:30 -0800

    Introduce a method for injecting D3D11 shader code.
    We're seeing far too much duplicated code between shaders now. In order to keep the level of madness down we'll introduce a fake include to inject common functionality in runtime HLSL compilations.

--- Source/Core_D3DCompiler.cpp
+++ Source/Core_D3DCompiler.cpp
@@ -5,11 +5,47 @@
 
 #pragma comment(lib, "d3dcompiler.lib")
 
+static const char Shader_D3D11_Common[] = R"SHADER(
+// This is the same cotangent reconstruction described at:
+// http://www.thetenthplanet.de/archives/1180
+float3x3 cotangent_frame( float3 N, float3 p, float2 uv ) {
+  // get edge vectors of the pixel triangle
+  float3 dp1 = ddx( p );
+  float3 dp2 = ddy( p );
+  float2 duv1 = ddx( uv );
+  float2 duv2 = ddy( uv );
+  // solve the linear system
+  float3 dp2perp = cross( dp2, N );
+  float3 dp1perp = cross( N, dp1 );
+  float3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+  float3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+  // construct a scale-invariant frame
+  float invmax = rsqrt( max( dot(T,T), dot(B,B) ) );
+  return float3x3( T * invmax, B * invmax, N );
+}
+)SHADER";
+
+class InjectedIncludes : public ID3DInclude {
+public:
+  HRESULT Open(D3D_INCLUDE_TYPE IncludeType, LPCSTR pFileName, LPCVOID pParentData, LPCVOID *ppData, UINT *pBytes) override {
+    if (IncludeType == D3D_INCLUDE_LOCAL && !strcmp(pFileName, "Sample_D3D11_Common.inc")) {
+      *ppData = Shader_D3D11_Common;
+      *pBytes = sizeof(Shader_D3D11_Common) - 1; // This will have a NULL terminator which will screw up parsing; remove it.
+      return S_OK;
+    }
+    return STG_E_FILENOTFOUND;
+  }
+  HRESULT Close(LPCVOID pData) override {
+    return S_OK;
+  }
+};
+
 ID3DBlob *CompileShader(const char *profile, const char *entrypoint,
                         const char *shader) {
   ID3DBlob *pD3DBlobCode = {};
   CComPtr<ID3DBlob> pD3DBlobError;
-  D3DCompile(shader, strlen(shader), nullptr, nullptr, nullptr, entrypoint,
+  InjectedIncludes includes;
+  D3DCompile(shader, strlen(shader), nullptr, nullptr, &includes, entrypoint,
              profile, 0, 0, &pD3DBlobCode, &pD3DBlobError);
   if (nullptr != pD3DBlobError) {
     throw std::exception(
--- Source/Sample_D3D11Scene.cpp
+++ Source/Sample_D3D11Scene.cpp
@@ -42,6 +42,8 @@ CreateSample_D3D11Scene(std::shared_ptr<Direct3D11Device> device,
   ////////////////////////////////////////////////////////////////////////////////
   // Create all the shaders that we might need.
   const char *szShaderCode = R"SHADER(
+#include "Sample_D3D11_Common.inc"
+
 cbuffer Constants
 {
     float4x4 TransformWorldToClip;
@@ -76,24 +78,6 @@ VertexPS mainVS(VertexVS vin)
     vout.Texcoord = vin.Texcoord;
     vout.WorldPosition = mul(TransformObjectToWorld, vin.Position).xyz;
     return vout;
-}
-
-// This is the same cotangent reconstruction described at:
-// http://www.thetenthplanet.de/archives/1180
-float3x3 cotangent_frame( float3 N, float3 p, float2 uv ) {
-  // get edge vectors of the pixel triangle
-  float3 dp1 = ddx( p );
-  float3 dp2 = ddy( p );
-  float2 duv1 = ddx( uv );
-  float2 duv2 = ddy( uv );
-  // solve the linear system
-  float3 dp2perp = cross( dp2, N );
-  float3 dp1perp = cross( N, dp1 );
-  float3 T = dp2perp * duv1.x + dp1perp * duv2.x;
-  float3 B = dp2perp * duv1.y + dp1perp * duv2.y;
-  // construct a scale-invariant frame
-  float invmax = rsqrt( max( dot(T,T), dot(B,B) ) );
-  return float3x3( T * invmax, B * invmax, N );
 }
 
 float4 mainPS(VertexPS vin) : SV_Target
