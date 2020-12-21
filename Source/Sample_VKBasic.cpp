c6174dcb1602885fbc3e24cad8f761fe875f496c
Adam Johnson <aj@nowhere.com>
2020-12-21 14:49:49 -0800

    Format Vulkan file.

--- Source/Sample_VKBasic.cpp
+++ Source/Sample_VKBasic.cpp
@@ -14,44 +14,36 @@
 #include <vector>
 #include <vulkan\vulkan.h>
 
-std::function<void(VKDevice*, vk::Image)>
-CreateSample_VKBasic() {
+std::function<void(VKDevice *, vk::Image)> CreateSample_VKBasic() {
 
   return [=](VKDevice *deviceVK, vk::Image imageBackbuffer) {
-
-
-
     // Perform a clear of the Vulkan image via a dispatched command buffer.
-    VK_Run_Synchronously(
-        deviceVK, [&](vk::CommandBuffer m_vkCommandBuffer) {
-          {
-            std::vector<vk::ImageMemoryBarrier> imb = {vk::ImageMemoryBarrier(
-                (vk::AccessFlagBits)0, (vk::AccessFlagBits)0,
-                vk::ImageLayout::eUndefined, vk::ImageLayout::eGeneral,
-                VK_QUEUE_FAMILY_IGNORED, VK_QUEUE_FAMILY_IGNORED, imageBackbuffer,
-                vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1,
-                                          0, 1))};
-            m_vkCommandBuffer.pipelineBarrier(
-                vk::PipelineStageFlagBits::eTopOfPipe,
-                vk::PipelineStageFlagBits::eTransfer, (vk::DependencyFlagBits)0,
-                {}, {}, imb);
-          }
-          {
-            static float colorStrobe = 0;
-            std::vector<vk::ImageSubresourceRange> isr = {
-                vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1,
-                                          0, 1)};
-            m_vkCommandBuffer.clearColorImage(
-                imageBackbuffer, vk::ImageLayout::eGeneral,
-                vk::ClearColorValue(std::array<float, 4>{colorStrobe, 0, 0, 1}),
-                isr);
-            colorStrobe += 0.1f;
-            if (colorStrobe > 1)
-              colorStrobe -= 1;
-          }
-        });
-
-
+    VK_Run_Synchronously(deviceVK, [&](vk::CommandBuffer m_vkCommandBuffer) {
+      {
+        std::vector<vk::ImageMemoryBarrier> imb = {vk::ImageMemoryBarrier(
+            (vk::AccessFlagBits)0, (vk::AccessFlagBits)0,
+            vk::ImageLayout::eUndefined, vk::ImageLayout::eGeneral,
+            VK_QUEUE_FAMILY_IGNORED, VK_QUEUE_FAMILY_IGNORED, imageBackbuffer,
+            vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1, 0,
+                                      1))};
+        m_vkCommandBuffer.pipelineBarrier(vk::PipelineStageFlagBits::eTopOfPipe,
+                                          vk::PipelineStageFlagBits::eTransfer,
+                                          (vk::DependencyFlagBits)0, {}, {},
+                                          imb);
+      }
+      {
+        static float colorStrobe = 0;
+        std::vector<vk::ImageSubresourceRange> isr = {vk::ImageSubresourceRange(
+            vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1)};
+        m_vkCommandBuffer.clearColorImage(
+            imageBackbuffer, vk::ImageLayout::eGeneral,
+            vk::ClearColorValue(std::array<float, 4>{colorStrobe, 0, 0, 1}),
+            isr);
+        colorStrobe += 0.1f;
+        if (colorStrobe > 1)
+          colorStrobe -= 1;
+      }
+    });
   };
 }
 
