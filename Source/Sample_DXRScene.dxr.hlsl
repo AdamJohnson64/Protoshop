81cfb88c31187a3592772b4dc03f6cc03c6b805c
Adam Johnson <aj@nowhere.com>
2021-04-22 15:37:25 -0700

    Switchable single-ray or AO shadowing.
    Sometimes we actually want framerate.

--- Source/Sample_DXRScene.dxr.hlsl
+++ Source/Sample_DXRScene.dxr.hlsl
@@ -58,25 +58,53 @@ void PrimaryMaterialTextured(inout RayPayload rayPayload, in IntersectionAttribu
   float2 duv1 = v1.Texcoord - v0.Texcoord;
   float2 duv2 = v2.Texcoord - v0.Texcoord;
   // Perform the interpolation of all components.
-  float3 position = v0.Position + dp1 * barya + dp2 * baryb;
-  float3 normal = v0.Normal + dn1 * barya + dn2 * baryb;
-  float2 uv = v0.Texcoord + duv1 * barya + duv2 * baryb;
+  float3 interpPosition = v0.Position + dp1 * barya + dp2 * baryb;
+  float3 interpNormal = normalize(v0.Normal + dn1 * barya + dn2 * baryb);
+  float2 interpUV = v0.Texcoord + duv1 * barya + duv2 * baryb;
   // Look up all textures using the computed texture coordinate.
-  float3 texelDiffuse = SampleTextureBilinear(textureDiffuse, uv).rgb;
-  float3 texelNormal = SampleTextureBilinear(textureNormal, uv).rgb;
+  float3 texelDiffuse = SampleTextureBilinear(textureDiffuse, interpUV).rgb;
+  float3 texelNormal = SampleTextureBilinear(textureNormal, interpUV).rgb;
 
   // Note: In the pixel shader version of this we ddx/ddy to produce a reference plane.
   // When we're raytracing it's nonsense to use ddx/ddy so we'll use the triangle directly.
   // solve the linear system
-  float3 dp2perp = cross( dp2, normal );
-  float3 dp1perp = cross( normal, dp1 );
+  float3 dp2perp = cross( dp2, interpNormal );
+  float3 dp1perp = cross( interpNormal, dp1 );
   float3 T = dp2perp * duv1.x + dp1perp * duv2.x;
   float3 B = dp2perp * duv1.y + dp1perp * duv2.y;
   // construct a scale-invariant frame
   float invmax = rsqrt( max( dot(T,T), dot(B,B) ) );
-  float3x3 matTangentFrame = float3x3( T * invmax, B * invmax, normal );
-  float3 normalBump = normalize(mul(texelNormal * 2 - 1, matTangentFrame));
+  float3x3 matTangentFrame = float3x3( T * invmax, B * invmax, interpNormal );
+
+  // Calculate the normal mapped normal.
+  float3 vectorNormal = normalize(mul(texelNormal * 2 - 1, matTangentFrame));
   
+// Flip this over to use a basic shadow rendering.
+#if 1
+  // Reconstuct an orthonormal frame from a constant axis.
+  float3 orthoB = normalize(cross(float3(1, 0, 0), vectorNormal));
+  float3 orthoT = normalize(cross(orthoB, vectorNormal));
+  float3x3 matTangentOrtho = float3x3(orthoT, orthoB, vectorNormal);
+
+  // Use this orthonormal frame to transform hemisphere samples.
+  float3 rayOrigin = WorldRayOrigin() + WorldRayDirection() * RayTCurrent() + interpNormal * 0.0001;
+  float3 irradiance = float3(0, 0, 0);
+  int sampleOffset = (DispatchRaysIndex().x * DispatchRaysIndex().y * 16);
+  for (int i = 0; i < 8; ++i)
+  {
+      // This incredibly cheap sample jittering of i + x * y * 16 produces a high-frequency
+      // Moire interference pattern that looks like old-school dithering; nice.
+      float3 hemisphere = HaltonSample(i + sampleOffset);
+      float3 hemisphereInTangentFrame = mul(hemisphere, matTangentOrtho);
+      RayDesc rayDesc = { rayOrigin + hemisphereInTangentFrame * 0.01, 0, hemisphereInTangentFrame, DEFAULT_TMAX };
+      RayPayload rayPayload;
+      rayPayload.Color = float3(0, 0, 0);
+      TraceRay(raytracingAccelerationStructure, RAY_FLAG_NONE, 0xFF, 1, 0, 1, rayDesc, rayPayload);
+      // Fudge factor of 10 to account for heavy shadow in Sponza.
+      irradiance += rayPayload.Color * 10;
+  }
+  rayPayload.Color = texelDiffuse * irradiance / 8;
+#else
   // Fire a single shadow ray to demonstrate hit group offsets.
   float3 lightOrigin = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
   float3 lightDirection = float3(-3, 1, 0.1) - lightOrigin;
@@ -85,9 +113,10 @@ void PrimaryMaterialTextured(inout RayPayload rayPayload, in IntersectionAttribu
   shadowRayPayload.Color = float3(0, 0, 0);
   TraceRay(raytracingAccelerationStructure, RAY_FLAG_FORCE_OPAQUE | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH, 0xFF, 1, 0, 1, rayDescShadow, shadowRayPayload);
   // Calculate a simple dot product lighting.
-  float illum = clamp(dot(normalBump, normalize(lightDirection)), 0, 1);
+  float illum = clamp(dot(vectorNormal, normalize(lightDirection)), 0, 1);
   // Emit the color with dot product ligiting and simple shadowing.
   rayPayload.Color = texelDiffuse * illum * shadowRayPayload.Color;
+#endif
 }
 
 [shader("miss")]
@@ -99,5 +128,5 @@ void ShadowMiss(inout RayPayload rayPayload)
 [shader("closesthit")]
 void ShadowMaterialTextured(inout RayPayload rayPayload, in IntersectionAttributes intersectionAttributes)
 {
-    rayPayload.Color = float3(0.25, 0.25, 0.25);
+    rayPayload.Color = float3(0, 0, 0);
 }
